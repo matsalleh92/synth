62c9785086060da19cbf0bb6cab015e973c4c7eb
matsalleh92 <elca@een-pc71.ds.leeds.ac.uk>
2015-04-23 19:32:22 +0100

    Sim works. Start of synth pop. Quartus closed.

--- constants.v
+++ constants.v
@@ -15,6 +15,8 @@
 `define HALF_SINE_WORDS ((2**`HALF_SINE_WIDTH)-1)
 //Params for the sine approximator
 `define SINE_WORDS 8191
+`define Q3_SINE 49152
+`define Q1_SINE 16384
 
 //Params for MIDI I/F
 `define KEY_WIDTH 7
@@ -24,4 +26,3 @@
 `define pac2sine_approx_t [`OSC_WIDTH-1:0]
 `define volt_t [`OSC_DEPTH-1:0]
 `define osc2pac_t [8:0]
-
--- synth.v
+++ synth.v
@@ -2,8 +2,21 @@
 
 //Top level for my really bad synth
 
-module synth (input clk);
-	//OTB oscillator_tb;
+module synth (input clk50,
+output fpga2dac
+);
 	
-	//oscillator_tb OTB();
+	//Instantiate Terrasic's terrible terrible DAC I/F
+	adio_codec ad0(
+	.oAUD_DATA(fpga2dac.data),
+	.oAUD_LRCK(fpga2dac.lrclk),
+	.oAUD_BCK(fpga2dac.bclk),
+	.iCLK_18_4(clk18_4),
+	.iRST_N(locked),
+	.sample(osc2dac)
+	);
+	
+	//Instantiate the PLL for generating clocks and stuff
+	
+	
 endmodule
